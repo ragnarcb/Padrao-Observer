e58780ee9614624f1b6dbe3156c0e5f5d218fe55
CarusoAugusto <103348874+ragnarcb@users.noreply.github.com>
2024-07-08 12:35:27 -0300

    ,

--- observerCorrigido/ChegadaAniversarianteEvent.java
+++ observerCorrigido/ChegadaAniversarianteEvent.java
@@ -5,10 +5,14 @@ import java.sql.Date;
 public class ChegadaAniversarianteEvent {
 
     private final Date horaDaChegada;
+
     public ChegadaAniversarianteEvent(Date horaDaChegada) {
+        super();
         this.horaDaChegada = horaDaChegada;
     }
+
     public Date getHoraDaChegada() {
         return horaDaChegada;
     }
+
 }
